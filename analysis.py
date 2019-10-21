5b6cc4aeef7094ad366ba738386d2722d33b02a0
Xacla <area184persona@gmail.com>
2019-10-21 14:57:15 +0900

    change_meteric_ _euclidean

--- analysis.py
+++ analysis.py
@@ -24,14 +24,14 @@ plt.scatter(result_pca[:,0],result_pca[:,1],edgecolors="k")
 import umap
 
 for i in range(5,100):
-    umap_ins=umap.UMAP(n_neighbors=i,metric='canberra',verbose=True,random_state=0,min_dist=0.1)
+    umap_ins=umap.UMAP(n_neighbors=i,metric='euclidean',verbose=True,random_state=0,min_dist=0.1)
     result_umap=umap_ins.fit_transform(data_np)
-    save_csv_name="./neighbor_exp/csv_files/neighbor_"+str(i)+".csv"
+    save_csv_name="./neighbor_eud/csv_files/neighbor_"+str(i)+".csv"
     np.savetxt(save_csv_name,result_umap,delimiter=",")
 
     plt.close()
     plt.scatter(result_umap[:,0],result_umap[:,1],edgecolors='k')
-    save_name="./neighbor_exp/image/neighbor_"+str(i)+".png"
+    save_name="./neighbor_eud/image/neighbor_"+str(i)+".png"
     plt.savefig(save_name)
 
 
